d950ffb32ab9a1be70fa64f0a5cb1af3b7442dc6
Prathyusha <90699228+P-Peddireddy@users.noreply.github.com>
2022-04-01 23:19:14 -0400

    Add files via upload

---
+++ MVCApp/MVCApp/Controllers/TestController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using MVCApp.Models;
+
+namespace MVCApp.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public TestController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+
+    }
+}
